f36ad4e5c85f85fc8df5c5614f71079897ab5e8d
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-23 18:09:20 -0700

    created an unordered list with a stateless component

---
+++ my-app/src/components/list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List(props){
+  return (
+    <ul>
+      <li>Cloud</li>
+      <li>Tifa</li>
+      <li>Aerith</li>
+    </ul>
+    )
+}
+
+export default List;
